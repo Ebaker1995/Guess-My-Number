665959d5f6d4c430bef2e30a12674ab55b8aa821
Eric Baker <ericbaker1995@gmail.com>
2022-02-27 18:35:46 -0500

    Second Update with colors

--- script.js
+++ script.js
@@ -21,11 +21,20 @@ let score = 20;
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
+  //When incorrect input
   if (!guess) {
     document.querySelector('.message').textContent = '😠 No number!';
+
+    //When player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct number!';
     document.querySelector('.number').textContent = secretNumber;
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    document.querySelector('.number').style.width = '30rem';
+
+    //When player is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📈 Guess too high';
@@ -34,15 +43,23 @@ document.querySelector('.check').addEventListener('click', function () {
     } else {
       document.querySelector('.message').textContent = '💣You lost the game';
       document.querySelector('.number').textContent = secretNumber;
+      document.querySelector('body').style.backgroundColor = '#FF0000';
+      document.querySelector('.number').style.width = '30rem';
     }
+
+    //When player is too low
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📉 Guess too low';
       score--;
       document.querySelector('.score').textContent = score;
+
+      //When player looses
     } else {
       document.querySelector('.message').textContent = '💣You lost the game';
       document.querySelector('.number').textContent = secretNumber;
+      document.querySelector('body').style.backgroundColor = '#FF0000';
+      document.querySelector('.number').style.width = '30rem';
     }
   }
 });
